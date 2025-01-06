f68e95559c5d80e16b8faac40dad74099fc84727
Eliott-B <eliottb.info@gmail.com>
2025-01-06 12:37:57 +0100

    :bug: :memo: | Remove grid_path + Change default value for start and end parameter

--- api/src/models/grid.py
+++ api/src/models/grid.py
@@ -8,7 +8,7 @@ class Grid:
         self,
         width: int,
         height: int,
-        start: tuple[int, int] = (-1, -1),
+        start: tuple[int, int] = (0, 0),
         end: tuple[int, int] = (-1, -1),
     ) -> None:
         """Create a grid.
@@ -16,11 +16,14 @@ class Grid:
         Args:
             width (int): Width of the grid.
             height (int): Height of the grid.
+            start (tuple[int, int], optional): Start position. Defaults to (0, 0).
+            end (tuple[int, int], optional): End position. Defaults to (width, height).
 
         """
+        if end == (-1, -1):
+            end = (width - 1, height - 1)
         self.width = width
         self.height = height
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
-        self.grid_path = [[0 for _ in range(width)] for _ in range(height)]
         self.start = start
         self.end = end
