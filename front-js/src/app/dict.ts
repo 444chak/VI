a28718666bbf955e706047400e4cf192750e9c0b
444chak <444chak@gmail.com>
2025-01-08 22:45:12 +0100

    :art: Ajouter des constantes de couleurs, des messages d'erreur et des labels d'algorithmes dans le fichier dict.ts

---
+++ front-js/src/app/dict.ts
@@ -0,0 +1,75 @@
+// Types et interfaces
+interface ColorMapping {
+  [key: string | number]: string | number;
+}
+
+// Constantes des couleurs
+export const COLORS: ColorMapping = {
+  EMPTY: "",
+  WALL: "black",
+  PATH: "lightgrey",
+  WATER: "blue",
+  GRASS: "green",
+  ICE: "lightblue",
+  START: "#afafaf",
+  END: "#9b1111",
+};
+
+// Valeurs associées aux couleurs
+export const COLOR_VALUES: ColorMapping = {
+  "": 2,
+  undefined: 2,
+  lightgrey: 2,
+  black: -1,
+  blue: 5,
+  green: 3,
+  lightblue: 1,
+  start: 0,
+  end: 0,
+};
+
+// Mapping des valeurs vers les couleurs
+export const VALUE_TO_COLOR: ColorMapping = {
+  "2": "lightgrey",
+  "-1": "black",
+  "5": "blue",
+  "3": "green",
+  "1": "lightblue",
+};
+
+// Messages d'erreur
+export const ERROR_MESSAGES = {
+  ALGO_IN_PROGRESS: "Veuillez attendre la fin de l'algorithme en cours",
+  GENERIC_ERROR: "Une erreur est survenue",
+  NO_PATH: "Aucun chemin trouvé",
+  INVALID_START: "Position de départ invalide",
+  INVALID_END: "Position d'arrivée invalide",
+};
+
+// Labels des algorithmes
+export const ALGORITHM_LABELS = {
+  DIJKSTRA: "Dijkstra",
+  ASTAR: "A*",
+  DFS: "DFS",
+  BFS: "BFS",
+  RESET: "Réinitialiser",
+};
+
+// Tooltips
+export const TOOLTIPS = {
+  DIJKSTRA: "Algorithme de Dijkstra",
+  ASTAR: "Heuristique de Manhattan",
+  DFS: "Parcours en profondeur",
+  BFS: "Parcours en largeur",
+};
+
+// États des cases
+export const CELL_STATES = {
+  EMPTY: 2,
+  WALL: -1,
+  WATER: 5,
+  GRASS: 3,
+  ICE: 1,
+  START: 0,
+  END: 0,
+};
