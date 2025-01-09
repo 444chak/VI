e0072b451a7086f1fe79546ba97dba1f896409d9
Eliott-B <eliottb.info@gmail.com>
2025-01-09 09:22:44 +0100

    :rotating_light: | Linter

--- api/src/algorithmes/dijkstra.py
+++ api/src/algorithmes/dijkstra.py
@@ -14,6 +14,7 @@ def dijkstra(
         tuple[list[tuple[int, int]], list[tuple[list[tuple[int, int]], int]]]:
             - Shortest path
             - List of (path, cost) for each exploration step
+
     """
     start = hexagon_grid.start
     end = hexagon_grid.end
@@ -28,7 +29,7 @@ def dijkstra(
 
         # Record current exploration step
         exploration_steps.append(
-            (get_path([pos for pos in current_path]), current_cost)
+            (get_path(list(current_path)), current_cost),
         )
 
         if current == end:
@@ -48,7 +49,7 @@ def dijkstra(
                 new_cost = current_cost + hexagon_grid.get_value(next_pos)
                 if next_pos not in cost_so_far or new_cost < cost_so_far[next_pos]:
                     cost_so_far[next_pos] = new_cost
-                    new_path = current_path + [next_pos]
+                    new_path = [*current_path, next_pos]
                     heappush(queue, (new_cost, next_pos, new_path))
                     came_from[next_pos] = current
                     visited.add(next_pos)
