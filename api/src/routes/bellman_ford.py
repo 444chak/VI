960fa5c81934ab814683e3afa5eb47ce42243b81
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2025-01-09 14:07:20 +0100

    Create router for bellman ford

---
+++ api/src/routes/bellman_ford.py
@@ -0,0 +1,28 @@
+"""Dijkstra route."""
+
+from fastapi import APIRouter, HTTPException
+
+from algorithmes.bellman_ford import bellman_ford
+from classes.hexagon import Hexagon
+from classes.hexagon_grid import HexagonGrid
+from models.grid import GridModel
+
+router = APIRouter()
+
+
+@router.post("/", summary="Bellman-Ford algorithm")
+async def bellman_route(grid: GridModel) -> dict:
+    """Bellman-Ford algorithm."""
+    start = Hexagon(*grid.start)
+    end = Hexagon(*grid.end)
+    grid_model = HexagonGrid(
+        width=len(grid.grid),
+        height=len(grid.grid[0]),
+        grid=grid.grid,
+    )
+    grid_model.set_start(start)
+    grid_model.set_end(end)
+    result = bellman_ford(grid_model)
+    if len(result) == 0:
+        raise HTTPException(status_code=400, detail="No path found.")
+    return {"result": result}
