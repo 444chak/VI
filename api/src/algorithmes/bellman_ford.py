fb70df77d6c32f7dc19cbfedb5b255a107822f59
Eliott-B <eliottb.info@gmail.com>
2025-01-09 14:13:52 +0100

    :rotating_light: Linter + Update route documentation for Bellman-Ford algorithm

--- api/src/algorithmes/bellman_ford.py
+++ api/src/algorithmes/bellman_ford.py
@@ -1,6 +1,7 @@
 """Bellman-Ford algorithm implementation for hexagonal grid."""
 
-from classes.hexagon_grid import Hexagon, HexagonGrid, get_path
+from classes.hexagon_grid import HexagonGrid, get_path
+
 
 def bellman_ford(hexagon_grid: HexagonGrid) -> list[tuple[int, int]]:
     """Bellman-Ford algorithm for hexagonal grid.
@@ -10,30 +11,32 @@ def bellman_ford(hexagon_grid: HexagonGrid) -> list[tuple[int, int]]:
 
     Returns:
         list[tuple[int, int]]: path from start to end or empty list if no path exists.
+
     """
     start = hexagon_grid.start
     end = hexagon_grid.end
     grid = hexagon_grid.grid
 
-    distance = {hex: float('inf') for hex in grid}
+    distance = {hexa: float("inf") for hexa in grid}
     distance[start] = 0
-    predecessor = {hex: None for hex in grid}
+    predecessor = {hexa: None for hexa in grid}
 
     for _ in range(len(grid) - 1):
-        for hex in grid:
-            neighbors = hexagon_grid.get_neighbors(hex)
+        for hexa in grid:
+            neighbors = hexagon_grid.get_neighbors(hexa)
             for neighbor in neighbors:
-                weight = hexagon_grid.get_cost(hex, neighbor)
-                if distance[hex] + weight < distance[neighbor]:
-                    distance[neighbor] = distance[hex] + weight
-                    predecessor[neighbor] = hex
+                weight = hexagon_grid.get_cost(hexa, neighbor)
+                if distance[hexa] + weight < distance[neighbor]:
+                    distance[neighbor] = distance[hexa] + weight
+                    predecessor[neighbor] = hexa
 
-    for hex in grid:
-        neighbors = hexagon_grid.get_neighbors(hex)
+    for hexa in grid:
+        neighbors = hexagon_grid.get_neighbors(hexa)
         for neighbor in neighbors:
-            weight = hexagon_grid.get_cost(hex, neighbor)
-            if distance[hex] + weight < distance[neighbor]:
-                raise ValueError("Graph contains a negative weight cycle")
+            weight = hexagon_grid.get_cost(hexa, neighbor)
+            if distance[hexa] + weight < distance[neighbor]:
+                msg = "Graph contains a negative weight cycle"
+                raise ValueError(msg)
 
     path = []
     current = end
