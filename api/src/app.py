67c5d9fa66675fb1a0cea2795e46eaf913633e91
Eliott-B <eliottb.info@gmail.com>
2025-01-09 14:12:53 +0100

    :bug: Fix import and routing for Bellman-Ford algorithm

--- api/src/app.py
+++ api/src/app.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routes import a_star, bfs, dfs, dijkstra
+from routes import a_star, bfs, dfs, dijkstra, bellman_ford
 
 app = FastAPI(title="VI API", version="1.0.0")
 
@@ -30,4 +30,4 @@ app.include_router(a_star.router, prefix="/astar", tags=["a_star"])
 app.include_router(dijkstra.router, prefix="/dijkstra", tags=["dijkstra"])
 app.include_router(bfs.router, prefix="/bfs", tags=["bfs"])
 app.include_router(dfs.router, prefix="/dfs", tags=["dfs"])
-app.include_router(bfs.router, prefix="/bellman_ford", tags=["belman-ford"])
+app.include_router(bellman_ford.router, prefix="/bellman_ford", tags=["belman-ford"])
