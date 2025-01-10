b01fec7dd5a0ea950e9d50e11352fd5197cf22bb
444chak <444chak@gmail.com>
2025-01-10 18:35:28 +0100

    :art: Ajouter la fonction setup_paths pour améliorer le traitement des chemins dans la route Bellman-Ford

--- api/src/routes/bellman_ford.py
+++ api/src/routes/bellman_ford.py
@@ -6,6 +6,7 @@ from algorithmes.bellman_ford import bellman_ford
 from classes.hexagon import Hexagon
 from classes.hexagon_grid import HexagonGrid
 from models.grid import GridModel
+from utils.paths_utils import setup_paths
 
 router = APIRouter()
 
@@ -25,4 +26,4 @@ async def bellman_route(grid: GridModel) -> dict:
     result, paths = bellman_ford(grid_model)
     if len(result) == 0:
         raise HTTPException(status_code=400, detail="No path found.")
-    return {"result": result, "paths": paths}
+    return {"result": result, "paths": setup_paths(paths)}
