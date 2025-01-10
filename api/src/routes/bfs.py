51b8ca98c17909d5b5c8370b65e3c09992808750
444chak <444chak@gmail.com>
2025-01-10 14:02:23 +0100

    :art: Supprimer les exceptions HTTP inutiles dans les routes BFS et Dijkstra

--- api/src/routes/bfs.py
+++ api/src/routes/bfs.py
@@ -1,6 +1,6 @@
 """BFS route."""
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from algorithmes.bfs import bfs
 from classes.hexagon import Hexagon
@@ -24,6 +24,4 @@ async def bfs_route(grid: GridModel) -> dict:
     grid_model.set_start(start)
     grid_model.set_end(end)
     result, paths = bfs(grid_model)
-    # if len(result) == 0:
-    #     raise HTTPException(status_code=400, detail="No path found.")
     return {"result": result, "paths": setup_paths(paths)}
--- api/src/routes/dijkstra.py
+++ api/src/routes/dijkstra.py
@@ -1,6 +1,6 @@
 """Dijkstra route."""
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from algorithmes.dijkstra import dijkstra
 from classes.hexagon import Hexagon
@@ -24,6 +24,4 @@ async def dijkstra_route(grid: GridModel) -> dict:
     grid_model.set_start(start)
     grid_model.set_end(end)
     result, paths = dijkstra(grid_model)
-    # if len(result) == 0:
-    #     raise HTTPException(status_code=400, detail="No path found.")
     return {"result": result, "paths": setup_paths(paths)}
