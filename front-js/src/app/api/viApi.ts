258af061826384d251fa9c6ed62f1c990ac21f17
444chak <444chak@gmail.com>
2025-01-11 13:38:09 +0100

    :bulb: Ajouter des commentaires pour la fonction getAlgorithm

--- front-js/src/app/api/viApi.ts
+++ front-js/src/app/api/viApi.ts
@@ -1,8 +1,14 @@
 import apiClient from "@/utils/apiClient";
 
+/**
+ * Requête POST pour obtenir les résultats d'un algorithme
+ * @param {string} name Nom de l'algorithme
+ * @param {object} params Paramètres de l'algorithme (grille, départ, arrivée)
+ * @returns Résultats de l'algorithme (chemin, chemins)
+ */
 export async function getAlgorithm(
   name: string,
-  params: { grid: number[][]; start: number[]; end: number[] },
+  params: { grid: number[][]; start: number[]; end: number[] }
 ) {
   try {
     const response = await apiClient.post(`/${name}/`, params);
