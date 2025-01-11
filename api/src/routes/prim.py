fe1b62fdaf2b31dc443cc7a9733706f72ec66a01
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2025-01-11 21:48:51 +0100

    Add router for prim

---
+++ api/src/routes/prim.py
@@ -0,0 +1,28 @@
+"""Prim route."""
+
+from fastapi import APIRouter, HTTPException
+
+from algorithmes.prim import prim
+from classes.hexagon import Hexagon
+from classes.hexagon_grid import HexagonGrid
+from models.grid import GridModel
+
+router = APIRouter()
+
+
+@router.post("/", summary="Prim algorithm")
+async def prim_route(grid: GridModel) -> dict:
+    """Prim algorithm."""
+    start = Hexagon(*grid.start)
+    end = Hexagon(*grid.end)
+    grid_model = HexagonGrid(
+        width=len(grid.grid),
+        height=len(grid.grid[0]),
+        grid=grid.grid,
+    )
+    grid_model.set_start(start)
+    grid_model.set_end(end)
+    result, paths = prim(grid_model)
+    if len(result) == 0:
+        raise HTTPException(status_code=400, detail="No path found.")
+    return {"result": result, "paths": paths}
