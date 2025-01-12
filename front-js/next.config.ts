ec6be6c6a30f3a40ebde9d6cb00cd457819a5dc5
444chak <444chak@gmail.com>
2025-01-12 12:37:54 +0100

    :art: Ajouter un Dockerfile pour la gestion des dépendances et la construction de l'application Next.js

--- front-js/next.config.ts
+++ front-js/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+};
+
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  ...nextConfig,
 };
 
 export default nextConfig;
